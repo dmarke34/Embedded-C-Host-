ddcb559148bf9a349b4f372418372cd1304013c0
dmarke34 <ajaparicio@cpp.edu>
2025-02-14 17:35:51 -0800

    Structure Padding

---
+++ dataAlignment/main.c
@@ -0,0 +1,15 @@
+
+
+#include<stdint.h>
+#include<stdio.h>
+
+int main(void) {
+
+	char  data1 = 0x11;
+	int   data2 = 0xFFFFEEEE;
+	char  data3 = 0x22;
+	short data4 = 0xABCD;
+
+	printf("Memory address          Content \n");
+	printf("=================================\n");
+}
