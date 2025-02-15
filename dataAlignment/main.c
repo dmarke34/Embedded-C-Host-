43dd198acc3c91223df7e8f53cd529d9319cd97e
dmarke34 <ajaparicio@cpp.edu>
2025-02-14 18:37:43 -0800

    Aligned Data Storage

--- dataAlignment/main.c
+++ dataAlignment/main.c
@@ -3,13 +3,35 @@
 #include<stdint.h>
 #include<stdio.h>
 
+struct DataSet {
+
+	char  data1;
+	int   data2;
+	char  data3;
+	short data4;
+};
+
 int main(void) {
 
-	char  data1 = 0x11;
-	int   data2 = 0xFFFFEEEE;
-	char  data3 = 0x22;
-	short data4 = 0xABCD;
+	struct DataSet data;
+
+	data.data1 = 0x11;
+	data.data2 = 0xFFFFEEEE;
+	data.data3 = 0x22;
+	data.data4 = 0xABCD;
+
+	uint8_t *ptr;
+
+	ptr = (uint8_t*)&data;
+
+	uint32_t totalSize = sizeof(struct DataSet);
 
 	printf("Memory address          Content \n");
 	printf("=================================\n");
+
+	for(uint32_t i = 0; i < totalSize; i++){
+
+		printf("%p    %X\n",ptr,*ptr);
+		ptr++;
+	}
 }
